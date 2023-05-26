7e00773f09a0d16b37a49c80a909937ddfdc4387
Jek-San <88803418+Jek-San@users.noreply.github.com>
2023-05-26 20:22:39 +0700

    initial commit
    oke

---
+++ src/components/Calendar/index.tsx
@@ -0,0 +1,69 @@
+import { FC, useState } from "react"
+import ReactCalendar from "react-calendar"
+import { add, format } from "date-fns"
+import { time } from "console"
+import {
+  INTERVAL,
+  STORE_CLOSING_TIME,
+  STORE_OPENING_TIME,
+} from "~/constant/config"
+
+interface indexProps {}
+
+const index: FC<indexProps> = ({}) => {
+  interface DataType {
+    justDate: Date | null
+    dateTime: Date | null
+  }
+  const [date, setDate] = useState<DataType>({
+    justDate: null,
+    dateTime: null,
+  })
+
+  console.log(date.dateTime)
+  function getTime() {
+    if (!date.justDate) return
+
+    const { justDate } = date
+    const beginning = add(justDate, { hours: STORE_OPENING_TIME })
+    const end = add(justDate, { hours: STORE_CLOSING_TIME })
+    const interval = INTERVAL
+
+    const times = []
+    for (let i = beginning; i <= end; i = add(i, { minutes: interval })) {
+      times.push(i)
+    }
+    return times
+  }
+  const times = getTime()
+
+  return (
+    <div className="flex h-screen flex-col items-center justify-center">
+      {date.justDate ? (
+        <div className="flex gap-4">
+          {times?.map((time, i) => (
+            <div key={`time-${i}`} className="rounded-sm bg-gray-100 p-2">
+              <button
+                type="button"
+                onClick={() => setDate((prev) => ({ ...prev, dateTime: time }))}
+              >
+                {format(time, "kk:mm")}
+              </button>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <ReactCalendar
+          minDate={new Date()}
+          className="REACT-CALENDAR p-2"
+          view="month"
+          onClickDay={(date) => {
+            setDate((prev) => ({ ...prev, justDate: date }))
+          }}
+        />
+      )}
+    </div>
+  )
+}
+
+export default index
